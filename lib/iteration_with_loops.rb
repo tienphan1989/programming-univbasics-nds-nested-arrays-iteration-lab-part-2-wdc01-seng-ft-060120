db5bb5039642e7148e92025adf8370bebb50a290
Tien Phan <tienphan1989@gmail.com>
2020-05-03 19:00:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,16 +4,10 @@ def find_min_in_nested_arrays(src)
   nested_min = []
 row_index = 0
 while row_index < src.count do
-  element_index = 0
-  min_temp = src[row_index][0]
-  while element_index < src[row_index].count do
-    if src[row_index][element_index] < min_temp
-      min_temp = src[row_index][element_index]
-      element_index += 1
-    end
-    nested_min.push(min_temp)
+  nested_min.push(src[row_index].min)
   row_index += 1
-  end
-  nested_min
 end
+nested_min
+end
+  
 
