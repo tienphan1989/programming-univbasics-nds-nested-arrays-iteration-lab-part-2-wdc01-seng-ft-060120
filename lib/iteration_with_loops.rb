d4fb868f2d970679bf73262e1706ec281ba3bd19
Tien Phan <tienphan1989@gmail.com>
2020-05-03 18:52:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,10 +9,11 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     if src[row_index][element_index] < min_temp
       min_temp = src[row_index][element_index]
+      element_index += 1
     end
-    element_index += 1
-  end
-  nested_min.push(min_temp)
+    nested_min.push(min_temp)
   row_index += 1
+  end
+  nested_min
 end
 
